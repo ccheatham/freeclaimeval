5ebe4377e631460ffc88bc28823d9044459bd016
Ched Cheatham <ched.cheatham@gmail.com>
2022-02-20 20:41:44 -0600

    adds tailwind

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
 import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg-gray-400 ">
+      <div className="h-1/2 grid">
+        <div className="p-6 max-w-sm mt-1-auto bg-white rounded-xl shadow-lg flex items-center space-x-4">
+          <div className="shrink-0">
+            <img className="h-12 w-12" src={logo} alt="ChitChat Logo" />
+          </div>
+          <div>
+            <div className="text-xl font-medium text-black">ChitChat</div>
+            <input type="text" />
+          </div>
+        </div>
+      </div>
+      <div className="min-h-screen grid place-items-center bg-violet-300">
+        <input type="text" />
+      </div>
     </div>
-  );
+  )
 }
-
 export default App;
