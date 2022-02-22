15998f58e753288a6d6d5bf3924e099082848296
Ched Cheatham <chedcheatham@Cheds-MacBook-Air.local>
2022-02-21 19:12:11 -0600

    hero

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
+import './App.css';
 import logo from './logo.svg';
+import background from "./bg.jpg";
 
 function App() {
   return (
-    <div className="">
+  <div
+    class="hero grid grid-cols-1 gap-0"
+  >
+    <div className="text-center w-96 mt-5 m-auto">
+      <h1 className="text-4xl font-sans text-zinc-100">
+        Baby Formula Linked To Necrotizing Enterocolitis in Premature Infants
+      </h1>
     </div>
+  </div>
   )
 }
 export default App;
