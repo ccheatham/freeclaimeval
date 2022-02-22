502eba12aadd24fce1728f0f744de9ecc9b3d2a1
Ched Cheatham <ched.cheatham@gmail.com>
2022-02-22 10:25:19 -0600

    oops: build fix

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import './App.css';
-import logo from './logo.svg';
-import background from "./bg.jpg";
 
 function App() {
   return (
   <div
-    class="hero grid grid-cols-1 gap-0"
+    className="hero grid grid-cols-1 gap-0"
   >
     <div className="text-center w-96 mt-5 m-auto">
       <h1 className="text-4xl font-sans text-zinc-100">
